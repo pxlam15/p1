2f1bf1900df550ec362ca0fa27f140a3eb41ba98
Peter Lam <pxlam15@gmail.com>
2014-09-18 16:31:44 -0400

    css did not upload correctly

--- p1.php
+++ p1.php
@@ -10,7 +10,7 @@
 
 	<body>
 		<center><h2>Peter Lam</h2>
-		<img src= "\images\Senior Pic.jpg" alt = "myHeadshot">
+		<img src= "images\Senior Pic.jpg" alt = "myHeadshot">
 
 		<h3>Project 2 Title</h3>
 		View:[link]
@@ -26,19 +26,23 @@
 		<div class = "info"><h4>About Me</h4>
 			<p>
 				My name is Peter Lamand I am a full time ALB student '15 concentrating in Psychology.  I have had the pleasure of coding in high school and worked on various independent projects in the past few years.  I hope to further practice my coding skills and expand my skillset.</p>
-
+		</div>
 		<div class = "info"><h4>Why I am taking DWA 15</h4>
 			<p>
 				I am hoping to take my web development skills to the next level and to learn via practical experience/applications and projects.</p>
+			</div>
 		<div class = "info"><h4>Previous Experience</h4>
 			<p>
 				My experience programming is primarily with Java.  My practical experience comes with doing some mobile applications and also teaching elementary schoolers to high schoolers the basics of Java, Python, and C#.</p>
+		</div>
 		<div class = "info"><h4>Mac or PC</h4>
 			<p>
 				I only work with PC only.  I do not have much experience with MAC.</p>
+		</div>
 		<div class = "info"><h4>Other Information About Me</h4>
 			<p>
 				I love learning new languages.  My dream is to travel the world practicing the martial art of its respective country (Example: Savatte, France, Muay Thai, Thailand, etc.)</p>
+		</div>
 	</body>
 
 </html>
