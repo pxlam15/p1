a23bc674b5a85a24c64be260c413f5a2b661b6cc
Peter Lam <pxlam15@gmail.com>
2014-09-18 16:12:52 -0400

    File paths again, added a css page because that is pretty important

--- p1.php
+++ p1.php
@@ -9,34 +9,35 @@
 
 	<body>
 		<center><h2>Peter Lam</h2>
-		<img src= "C:\xampp\htdocs\p1\images\Senior Pic.jpeg" alt = "myHeadshot">
+		<img src= "\images\Senior_Pic.jpeg" alt = "myHeadshot">
 
 		<h3>Project 2 Title</h3>
+		View:[link]
+		Github:[link]
 		<h3>Project 3 Title</h3>
+		View:[link]
+		Github:[link]
 		<h3>Project 4 Title</h3>
+		View:[link]
+		Github:[link]
 		</center>
 
-		<div class = "info" id"summary"><h4>About Me</h4>
+		<div class = "info"><h4>About Me</h4>
 			<p>
-				My name is Peter Lamand I am a full time ALB student '15 concentrating in Psychology.  I have had the pleasure of coding in high school and worked on various independent projects in the past few years.  I hope to further practice my coding skills and expand my skillset.
-			</p>
+				My name is Peter Lamand I am a full time ALB student '15 concentrating in Psychology.  I have had the pleasure of coding in high school and worked on various independent projects in the past few years.  I hope to further practice my coding skills and expand my skillset.</p>
 
-		<div class = "info" id"whyThisCourse"><h4>Why I am taking DWA 15</h4>
+		<div class = "info"><h4>Why I am taking DWA 15</h4>
 			<p>
-				I am hoping to take my web development skills to the next level and to learn via practical experience/applications and projects.
-			</p>
-		<div class = "info" id"prevEXP"><h4>Previous Experience</h4>
+				I am hoping to take my web development skills to the next level and to learn via practical experience/applications and projects.</p>
+		<div class = "info"><h4>Previous Experience</h4>
 			<p>
-				My experience programming is primarily with Java.  My practical experience comes with doing some mobile applications and also teaching elementary schoolers to high schoolers the basics of Java, Python, and C#.
-			</p>
-		<div class = "info" id"pcMasterRace"><h4>Mac or PC</h4>
+				My experience programming is primarily with Java.  My practical experience comes with doing some mobile applications and also teaching elementary schoolers to high schoolers the basics of Java, Python, and C#.</p>
+		<div class = "info"><h4>Mac or PC</h4>
 			<p>
-				I only work with PC only.  I do not have much experience with MAC.
-			</p>
-		<div class = "info" id"other"><h4>Other Information About Me</h4>
+				I only work with PC only.  I do not have much experience with MAC.</p>
+		<div class = "info"><h4>Other Information About Me</h4>
 			<p>
-				I love learning new languages.  My dream is to travel the world practicing the martial art of its respective country (Example: Savatte, France, Muay Thai, Thailand, etc.)
-			</p>
+				I love learning new languages.  My dream is to travel the world practicing the martial art of its respective country (Example: Savatte, France, Muay Thai, Thailand, etc.)</p>
 	</body>
 
 </html>
